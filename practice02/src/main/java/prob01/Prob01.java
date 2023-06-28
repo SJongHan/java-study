2f63cd4b5c1d020ae7caf617e1d87cdbc5178420
PC21 <PC21@DESKTOP-MJVFQRQ>
2023-06-28 10:03:25 +0900

    practice02

--- practice02/src/main/java/prob01/Prob01.java
+++ practice02/src/main/java/prob01/Prob01.java
@@ -9,6 +9,12 @@ public class Prob01 {
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
 
 		/* 코드 작성 */
+		System.out.println("금액:");
+		int num  = scanner.nextInt();
+		for( int i = 0; i < MONEYS.length; i++) {
+			System.out.println(MONEYS[i]+ "원 : " + num/MONEYS[i]);
+				num%= MONEYS[i];
+		}
 		
 		scanner.close();
  	}
