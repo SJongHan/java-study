723631a93dcb3d050a9e0744f8c0062f3c28519a
PC21 <PC21@DESKTOP-MJVFQRQ>
2023-06-28 10:03:09 +0900

    다형성 - 오버로딩

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -3,13 +3,15 @@ package paint;
 public class Main {
 
 	public static void main(String[] args) {
-		Point point = new Point();
-		point.setX(100);
-		point.setY(50);
-		drawPoint(point);
+		Point point1 = new Point();
+		point1.setX(100);
+		point1.setY(50);
+		drawPoint(point1);
+		point1.show(false);
 		
 		Point point2 = new Point(200, 150);
 		drawPoint(point2);
+		point2.show(false);
 	}
 	
 	public static void drawPoint(Point point) {
