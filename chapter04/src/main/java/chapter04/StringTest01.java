59420e3fde2dd3c8bb86bb9573b4241fba8125a0
PC21 <PC21@DESKTOP-MJVFQRQ>
2023-06-29 14:10:34 +0900

    String Class I

---
+++ chapter04/src/main/java/chapter04/StringTest01.java
@@ -0,0 +1,27 @@
+package chapter04;
+
+import javax.swing.plaf.synth.SynthScrollBarUI;
+
+public class StringTest01 {
+
+	public static void main(String[] args) {
+		// c:\temp
+		
+		
+		System.out.println("c:\\temp");
+		
+		// "hello
+		System.out.println("\"hello\"");
+		
+		// \t: tab
+		// \r: carrige return
+		// \n: newline
+		// \b: beep
+				
+		// 
+		char c = '\'';
+		System.out.println(c);
+
+	}
+
+}
